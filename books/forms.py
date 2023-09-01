c50983d48d0d4a78aba26338dcded65e633481af
Bhawana <thawkarbhawna@gmail.com>
2023-09-01 11:49:57 +0530

    first commit

---
+++ books/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Book
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'published_date']
+        widgets = {
+            'published_date': forms.DateInput(attrs={'placeholder': 'YYYY-MM-DD'})}
